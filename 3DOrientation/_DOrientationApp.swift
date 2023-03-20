a68a20e573866cb2afa61c3a6e9fa6112dc9e20a
Joshua Yang <joshuayang@mac.com>
2023-03-20 15:20:49 +0800

    Initial Commit

---
+++ 3DOrientation/_DOrientationApp.swift
@@ -0,0 +1,17 @@
+//
+//  _DOrientationApp.swift
+//  3DOrientation
+//
+//  Created by Joshua Yang on 2023/3/20.
+//
+
+import SwiftUI
+
+@main
+struct _DOrientationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
